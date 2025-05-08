58fb590a497806573467eba47690530fa5f5a382
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-05-08 09:44:44 -0300

    create and implement QrCode generator service

--- src/main/java/com/devsouzx/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/devsouzx/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.devsouzx.qrcode.generator.controller;
 
 import com.devsouzx.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.devsouzx.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.devsouzx.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
